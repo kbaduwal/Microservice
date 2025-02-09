6f5fca12ece1f510b84b0215e8ceb566bc9c5455
kbaduwal <khakendra1999@gmail.com>
2025-02-09 16:02:41 +0545

    Implementation of API Gateway.

--- KJ_Uer_Authentican_Service/src/main/java/com/kj_uer_authentican_service/controller/TestController.java
+++ KJ_Uer_Authentican_Service/src/main/java/com/kj_uer_authentican_service/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.kj_uer_authentican_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+
+    @Autowired
+    private Environment env;
+
     @GetMapping("/hello")
     public String getGreeting() {
-        return "Hello World";
+        String port = env.getProperty("server.port");
+        return "Hello World (" + port + ")";
     }
 }
